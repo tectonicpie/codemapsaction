8691912b97edbb6dd6f035d8e11cd06b215f9f7b
mango <findmuizz@hotmail.com>
2022-10-30 19:29:42 +0000

    action updated
    Signed-off-by: mango <findmuizz@hotmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 var fs = require('fs');
+var zipper = require('zip-local');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -11,6 +12,8 @@ try {
   // console.log(`The event payload: ${payload}`);
   let path = core.getInput('path');
   console.log(path);
+  zipper.sync.zip(path).compress().save("pack.zip");
+  
   var files = fs.readdirSync(path);
   console.log(files);
   fs.readFile(path + "/" + files[4], 'utf8', (err, data) => {
