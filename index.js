ce1a70dc959b8e9158e0326bcfb747bf069e629b
mango <findmuizz@hotmail.com>
2022-10-30 14:30:38 +0000

    updated action
    Signed-off-by: mango <findmuizz@hotmail.com>

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+var fs = require('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
   // const nameToGreet = core.getInput('who-to-greet');
   console.log(`Starting action!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
-  let zippedFolder = core.getInput('zipped-folder');
-  console.log(zippedFolder);
+  let path = core.getInput('path');
+  console.log(path);
+  var files = fs.readdirSync(path);
+  console.log(files);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
 } catch (error) {
   core.setFailed(error.message);
 }
