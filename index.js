5c8b6d707ace996bc2a9b7908c76e0beaa9804ae
mango <findmuizz@hotmail.com>
2022-10-30 14:05:37 +0000

    logging zipped folder
    Signed-off-by: mango <findmuizz@hotmail.com>

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
-  const zippedFolder = core.getInput('zipped-folder');
-  console.log(`Zipped repo folder: ${zippedFolder}`);
+  let zippedFolder = core.getInput('zipped-folder');
+  console.log(zippedFolder);
 } catch (error) {
   core.setFailed(error.message);
 }
