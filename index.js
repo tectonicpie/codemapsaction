61753c829afbce6bf13fcd4d3ac97553b044adb8
mango <findmuizz@hotmail.com>
2022-10-30 14:01:02 +0000

    updated action js
    Signed-off-by: mango <findmuizz@hotmail.com>

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
   const zippedFolder = core.getInput('zipped-folder');
-  console.log(`Zipped folder: ${zippedFolder}`);
+  console.log(`Zipped repo folder: ${zippedFolder}`);
 } catch (error) {
   core.setFailed(error.message);
 }
